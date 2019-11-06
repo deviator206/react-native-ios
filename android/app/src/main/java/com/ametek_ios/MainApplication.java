707ee5b36e910ae638a2c5f9781d82f70adc1352
ue <ue@ues-Mac-mini.local>
2019-11-06 23:44:00 +0530

    changes for app

--- android/app/src/main/java/com/ametek_ios/MainApplication.java
+++ android/app/src/main/java/com/ametek_ios/MainApplication.java
@@ -3,6 +3,7 @@ package com.ametek_ios;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.swmansion.rnscreens.RNScreensPackage;
 import com.swmansion.reanimated.ReanimatedPackage;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new AsyncStoragePackage(),
             new VectorIconsPackage(),
             new RNScreensPackage(),
             new ReanimatedPackage(),
